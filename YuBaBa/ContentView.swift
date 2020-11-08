96a0dc65c5e0c29353e64ec94454e039198993f1
touyou <fujiyou1121@gmail.com>
2020-11-08 11:32:11 +0900

    Initial Commit

---
+++ YuBaBa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YuBaBa
+//
+//  Created by 藤井陽介 on 2020/11/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YuBaBa/YuBaBaApp.swift
@@ -0,0 +1,17 @@
+//
+//  YuBaBaApp.swift
+//  YuBaBa
+//
+//  Created by 藤井陽介 on 2020/11/08.
+//
+
+import SwiftUI
+
+@main
+struct YuBaBaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
